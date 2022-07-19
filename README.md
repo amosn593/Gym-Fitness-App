079f0f061d02c5451a6d68d231596908daeb2fde
Amos Ndonga <amosndonga@gmail.com>
2022-07-19 14:13:17 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gym Fitness Application
+
+#### Build with ReactJs, Material UI and Rapid API.
+
+![React Fitness Application](https://i.ibb.co/Yt9spGc/image.png)
+
+
---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+    "X-RapidAPI-Key": "c96cd132demshec7a32c834b53d8p174d79jsn9a1e1d165880",
+  },
+};
+
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+    "X-RapidAPI-Key": "c96cd132demshec7a32c834b53d8p174d79jsn9a1e1d165880",
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
